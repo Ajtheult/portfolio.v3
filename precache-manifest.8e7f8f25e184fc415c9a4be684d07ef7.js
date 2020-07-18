9989887e65d31650dd507081be2a8f84fa108403
Ajtheult <asaphfranksboi@gmail.com>
2020-07-18 10:50:25 +0300

    Updates

---
+++ precache-manifest.8e7f8f25e184fc415c9a4be684d07ef7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61af05ef73c00e2338befdb35dd10fe8",
+    "url": "/portfolio-v3/index.html"
+  },
+  {
+    "revision": "98d1a3ec26495382987c",
+    "url": "/portfolio-v3/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "d46a3d9d3480cc17e2ab",
+    "url": "/portfolio-v3/static/css/main.de1ca050.chunk.css"
+  },
+  {
+    "revision": "98d1a3ec26495382987c",
+    "url": "/portfolio-v3/static/js/2.3a0a00b8.chunk.js"
+  },
+  {
+    "revision": "b013b7cd7a2da92c16a2d9932cfeb76c",
+    "url": "/portfolio-v3/static/js/2.3a0a00b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46a3d9d3480cc17e2ab",
+    "url": "/portfolio-v3/static/js/main.5b2a7c02.chunk.js"
+  },
+  {
+    "revision": "5411b7e53e7c0332400e",
+    "url": "/portfolio-v3/static/js/runtime-main.c936e449.js"
+  },
+  {
+    "revision": "237871878d949034fdc521bef4d6e289",
+    "url": "/portfolio-v3/static/media/Contact.23787187.png"
+  },
+  {
+    "revision": "d39c050075a110bb46070fd1fbed8b96",
+    "url": "/portfolio-v3/static/media/Github.d39c0500.png"
+  },
+  {
+    "revision": "9ba17c49f27789b194da077659eb74c0",
+    "url": "/portfolio-v3/static/media/LL.9ba17c49.png"
+  },
+  {
+    "revision": "5072841b13c3bf05f253e749bd03f07b",
+    "url": "/portfolio-v3/static/media/Tech.5072841b.png"
+  },
+  {
+    "revision": "31d50da57b950c03cde5bd80ba5c71ff",
+    "url": "/portfolio-v3/static/media/background1.31d50da5.jpg"
+  },
+  {
+    "revision": "786f69d622866b36e8131b89a0b072d1",
+    "url": "/portfolio-v3/static/media/background2.786f69d6.jpg"
+  },
+  {
+    "revision": "6d6e0bab887c701ebcd1fa8a93a41b02",
+    "url": "/portfolio-v3/static/media/background3.6d6e0bab.jpg"
+  },
+  {
+    "revision": "d98e83f3801089cd897cb6f42193a2e2",
+    "url": "/portfolio-v3/static/media/me.d98e83f3.jpg"
+  },
+  {
+    "revision": "d9df28d6e0e1cc577260eff8edf37139",
+    "url": "/portfolio-v3/static/media/me.d9df28d6.jpeg"
+  },
+  {
+    "revision": "1de8072a13e4a397fbcfc7a4d4a72845",
+    "url": "/portfolio-v3/static/media/me2.1de8072a.jpg"
+  },
+  {
+    "revision": "42e0194103d64c725d4a1182381b643e",
+    "url": "/portfolio-v3/static/media/music.42e01941.jpg"
+  }
+]);
