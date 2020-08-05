2355972bb476ab3ac270fb925c1322c1f107a552
Ajtheult <asaphfranksboi@gmail.com>
2020-08-05 10:34:56 +0300

    final

--- .history/src/components/layout/pages/Home_20200805103336.js
+++ .history/src/components/layout/pages/Home_20200805103336.js
@@ -16,7 +16,7 @@ export const Home = () => {
   return (
     <div className="colour_of_about_text">
       <Parallax bgImage={background1} strength={400}>
-        <div style={{ height: 710 }}>
+        <div style={{ height: 700 }}>
           <div className="style_for_landingpage">
             <h1 className="intro_name_top">Asaph Franks</h1>
             <h4 className="intro_name_bottom">Frontend Developer</h4>
